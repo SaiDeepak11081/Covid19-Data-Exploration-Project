32bf001070f49157382a668d6ea4a3094f526fdc
Sai Deepak <SaiDeepak11081@gmail.com>
2025-08-07 07:59:28 +0530

    Updated Covid Deaths Data Exploration Portfolio Project SQL.sql

--- Covid Deaths Data Exploration Portfolio Project SQL.sql
+++ Covid Deaths Data Exploration Portfolio Project SQL.sql
@@ -20,7 +20,7 @@ order by 1,2
 
 
 -- Total Cases vs Total Deaths
--- Shows likelihood of dying if you contract covid in your country
+-- Shows likelihood of dying if you contact covid in your country
 
 Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 From PortfolioProject..CovidDeaths
@@ -157,4 +157,5 @@ Join PortfolioProject..CovidVaccinations vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null 
+
 
